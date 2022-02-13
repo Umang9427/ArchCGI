78313f348c4ba004b95bc04ae855e50944fab769
Umang Bhalani <umang9427@gmail.com>
2022-02-13 19:40:59 +0530

    ArchCGI's First Commit. Copied from TGBArchStudio.

---
+++ OwlCarousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      items:2,
+     autoplay:12000,
+      margin:30,
+      loop:true,
+      dots:true
+//      nav:true,
+//      navText:["<i class='fas fa-long-arrow-alt-left'></i>","<i class='fas fa-long-arrow-alt-right'></i>" ]
+  });
+});
