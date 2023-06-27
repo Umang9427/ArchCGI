c03ceb04f26c67df5edca6dcc473fd0aa106ff6b
Umang9427 <umang9427@gmail.com>
2023-06-27 19:54:40 +0530

    Portfolio card UI Change

--- index.js
+++ index.js
@@ -202,35 +202,35 @@ function reveal() {
 
 
 /*------------------------------ Animated Text header------------------------------- */
-document.getElementById("mainwords").style.animation="Umang 15s ease-in-out infinite";
+document.getElementById("mainwords").style.animation="Umang 24s ease-in-out infinite";
 
 function doSetTimeout(){
   setTimeout(function() {
    document.getElementById('mainwords').innerHTML = '<span>Top Notch Detailing in Architectural Visualization</span>';
    doSetTimeout2();
 
-  }, 3750);
+  }, 6000);
 }
 function doSetTimeout2(){
   setTimeout(function() {
    document.getElementById('mainwords').innerHTML = '<span>Bringing 3D Rendering to a Vast Majority Globally</span>';
    doSetTimeout3();
 
-  }, 3750);
+  }, 6000);
 }
 function doSetTimeout3(){
   setTimeout(function() {
    document.getElementById('mainwords').innerHTML = '<span>Expert 3D Interior | Exterior | Landscape Design</span>';
    doSetTimeout4();
 
-  }, 3750);
+  }, 6000);
 }
 function doSetTimeout4(){
   setTimeout(function() {
    document.getElementById('mainwords').innerHTML = '<span>Industry Leading Showcase of CGI for Architecture</span>';
    doSetTimeout();
 
-  }, 3750);
+  }, 6000);
 }
 
 
